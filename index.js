73ead744b1f06a10168be3e01a46453d8ac45f52
shengxinjing <316783812@qq.com>
2014-12-03 14:11:35 +0800

    add index.js
    add index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+ * shengxinjing
+ *
+ */
+
+'use strict';
+
+var ngAnnotate = require("ng-annotate");
+var semi = require('semi');
+
+module.exports = function(content, file, conf) {
+    if (!conf.annotate || conf.annotate == 'add') {
+        content = ngAnnotate(content, {
+            add: true
+        }).src;
+    } else if (conf.annotate == 'remove') {
+        content = ngAnnotate(content, {
+            remove: true
+        }).src;
+    }
+    if (!conf.semi || conf.semi == 'add') {
+        content = semi.add(content);
+    } else if (conf.semi == 'remove') {
+        content = semi.remove(content);
+    }
+    return content;
+};
